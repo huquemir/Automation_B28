ea223c2b99634a613e96c6ebacc9be0a4803622d
smarttech <smarttech@10.211.55.3>
2022-09-25 14:53:05 -0400

    My first automation upload

---
+++ Techshop_Project_B28/src/main/java/com/smarttechqa/basepage/BaseClass.java
@@ -0,0 +1,36 @@
+package com.smarttechqa.basepage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	
+	public static void setUp(){
+		
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.smarttechqa.com/login");
+		
+		//driver.findElement(By.id("email")).sendKeys("testuser@email.com");
+		driver.findElement(By.xpath("//*[@type='email']")).sendKeys("testuser@email.com");
+		driver.findElement(By.id("password")).sendKeys("123456");
+		//driver.findElement(By.cssSelector("#password")).sendKeys("123456");
+		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+		System.out.println("The title of the application is : "+driver.getTitle()); //To verify if you logged in 
+		driver.quit();
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		BaseClass.setUp();
+		
+	}
+	
+
+}
